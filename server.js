b67932f61e9a5f30f71b345f834ac8bc8de898c7
snw380 <ty.morgan818@gmail.com>
2020-11-12 21:33:17 -0500

    added server fix

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ app.use(routes);
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
 // Start the API server
